f5d7953a1ca4cc4d294761022f6e99d1d442053e
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-01-28 10:57:29 -0300

    Contribuidores em maiúsculo

--- pages/quizBoleiro.jsx
+++ pages/quizBoleiro.jsx
@@ -1,7 +1,7 @@
 import db from '../db.json';
 import CakeText from '../src/components/CakeText/index';
 import QuizBackground from '../src/components/QuizBackground/index';
-import { QuizContainer } from './contribuidores/index';
+import { QuizContainer } from './Contribuidores/index';
 import QuizLogo from '../src/components/QuizLogo/index';
 import Widget from '../src/components/Widget/index';
 
@@ -25,7 +25,13 @@ const QuizBoleiro = () => (
           <CakeText>Questão 02</CakeText>
         </Widget.Header>
         <Widget.Content>
-          <p>Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.</p>
+          <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan
+            disputationi eu sit. Vide electram sadipscing et per.
+            Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis.
+            Cevadis im ampola pa arma uma pindureta.
+
+          </p>
         </Widget.Content>
       </Widget>
 
